04183d31ede63d0bbe3f76f99dc01e17b5e7f731
pertro-cat <150966625+pertro-cat@users.noreply.github.com>
2023-11-21 13:10:40 +0200

    add lab 1

---
+++ lab1.py
@@ -0,0 +1,15 @@
+sport = "footbal"
+print(sport)
+is_student = 'True'
+is_teacher = 'False'
+is_student == is_teacher
+result = is_student == is_teacher
+is_hot = 'False'
+is_cold = 'True'
+result = is_cold or is_hot
+
+print("cvbnngfdcfbhh"[2:6])
+
+
+
+
